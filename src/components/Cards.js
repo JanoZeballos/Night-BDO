f19db4f70fe3ba34460d55ce091441645b6ac8ac
JanoZeballos <49873953+JanoZeballos@users.noreply.github.com>
2020-12-21 17:16:54 -0300

    Card Responsive and Style

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,29 +6,37 @@ function Gallery() {
         <div className='cards-container' id='cards'>
             <ul className="cards">
                 <li className="cards_item">
+                    <a href='/'>
                     <div className="card">
-                        <img className='background' src='/images/background.jpg' alt='Background' />
+                        <img className='card_background' src='/images/khan.png' alt='Background' />
                         <div className="card_content">
                             <h2 className="card_title">KHAN</h2>
                             <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
                         </div>
                     </div>
+                    </a>
                 </li>
                 <li className="cards_item">
+                    <a href='/'>
                     <div className="card">
+                        <img className='card_background' src='/images/vell.jpg' alt='Background' />
                         <div className="card_content">
                             <h2 className="card_title">VELL</h2>
                             <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
                         </div>
                     </div>
+                    </a>
                 </li>
                 <li className="cards_item">
+                    <a href='/'>
                     <div className="card">
+                        <img className='card_background' src='/images/night.png' alt='Background' />
                         <div className="card_content">
                             <h2 className="card_title">NIGHT</h2>
                             <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
                         </div>
                     </div>
+                    </a>
                 </li>
             </ul>
         </div>
