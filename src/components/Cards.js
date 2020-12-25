c8ee832551fdb781f1b140db389725c1c5f9e2b0
JanoZeballos <49873953+JanoZeballos@users.noreply.github.com>
2020-12-25 15:08:56 -0300

    Fix cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,8 +3,13 @@ import './Cards.css'
 
 function Cards() {
     return (
+        <>
+        <div className='titles-container'>
+            <h1 className='title'>Informacion</h1>
+            <div class="fade_line"></div> 
+            <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
         <div className='cards-container' id='cards'>
-            <img className='background_cards' src='/images/background_cards.jpg' alt='Card_1' />
             <ul className="cards">
                 <li className="cards_item">
                     <a href='/'>
@@ -12,17 +17,6 @@ function Cards() {
                         <img className='card_background' src='/images/khan.png' alt='Card_1' />
                         <div className="card_content">
                             <h2 className="card_title">KHAN</h2>
-                            <p className="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
-                        </div>
-                    </div>
-                    </a>
-                </li>
-                <li className="cards_item">
-                    <a href='/'>
-                    <div className="card">
-                        <img className='card_background' src='/images/vell.jpg' alt='Card_2' />
-                        <div className="card_content">
-                            <h2 className="card_title">VELL</h2>
                             <p className="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
                         </div>
                     </div>
@@ -39,8 +33,20 @@ function Cards() {
                     </div>
                     </a>
                 </li>
+                <li className="cards_item">
+                    <a href='/'>
+                    <div className="card">
+                        <img className='card_background' src='/images/vell.jpg' alt='Card_2' />
+                        <div className="card_content">
+                            <h2 className="card_title">VELL</h2>
+                            <p className="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+                        </div>
+                    </div>
+                    </a>
+                </li>
             </ul>
         </div>
+        </>
     )
 }
 
